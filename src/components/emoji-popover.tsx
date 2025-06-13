cedf950c3139adc4a3c98c744cb09d7efb6ea316
Satyam Banwale <satyambanwale786@gmail.com>
2025-06-13 12:42:10 +0530

    added emoji picker react instead of emoji-mart

--- src/components/emoji-popover.tsx
+++ src/components/emoji-popover.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import Picker from "@emoji-mart/react";
-import data from "@emoji-mart/data";
+import EmojiPicker, { type EmojiClickData } from "emoji-picker-react";
 
 import {
   Tooltip,
@@ -13,7 +12,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 interface EmojiPopoverProps {
   children: React.ReactNode;
   hint?: string;
-  onEmojiSelect: (emoji: any) => void;
+  onEmojiSelect: (value: string) => void;
 }
 
 export const EmojiPopover = ({
@@ -24,8 +23,8 @@ export const EmojiPopover = ({
   const [popoverOpen, setPopoverOpen] = useState(false);
   const [tooltipOpen, setTooltipOpen] = useState(false);
 
-  const onSelect = (emoji: any) => {
-    onEmojiSelect(emoji);
+  const onSelect = (value: EmojiClickData) => {
+    onEmojiSelect(value.emoji);
     setPopoverOpen(false);
 
     setTimeout(() => {
@@ -49,7 +48,7 @@ export const EmojiPopover = ({
           </TooltipContent>
         </Tooltip>
         <PopoverContent className="p-0 w-full border-none shadow-none">
-          <Picker data={data} onEmojiSelect={onSelect} />
+          <EmojiPicker onEmojiClick={onSelect} />
         </PopoverContent>
       </Popover>
     </TooltipProvider>
